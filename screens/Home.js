5c45ea886410d80956b8b800d0b149a99441250b
Jacquelyn Elder <jacquelyn07@mac.com>
2021-03-24 15:41:32 -0500

    Create basic tile component

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,24 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 function Home({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home</Text>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Welcome to Elder Apothecary</Text>
+        <Text>Find your favorite Apothecary products and more</Text>
+        <Button title='Learn More' />
+      </View>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Products you can Trust</Text>
+        <Text>Search multiple apothecaries for products you trust</Text>
+        <Button title='Learn More' />
+      </View>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Why Elder Apothecary?</Text>
+        <Text>It's simple! We care about your health and your time.</Text>
+        <Button title='Learn More' />
+      </View>
     </View>
   );
 }
