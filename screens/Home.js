f6e2b9b5df0c1e0248cb705238f32a93313075ad
Jacquelyn Elder <jacquelyn07@mac.com>
2021-03-24 16:56:44 -0500

    Add updated image to card two

--- screens/Home.js
+++ screens/Home.js
@@ -1,10 +1,10 @@
 import * as React from 'react';
-import { View, Text, Button } from 'react-native';
+import { ScrollView, Text, Button, ScrollScrollView } from 'react-native';
 import { Card } from 'react-native-elements';
 
 function Home({ navigation }) {
   return (
-    <View style={{ marginTop: 30 }}>
+    <ScrollView style={{ marginTop: 20 }}>
       <Card>
         <Card.Image
           source={{
@@ -20,7 +20,7 @@ function Home({ navigation }) {
         <Card.Image
           source={{
             uri:
-              '/Users/jacquelynhagman/workspaces/gitrepos/elder-apothecary/screens/carousel-1800-2 .jpg',
+              '/Users/jacquelynhagman/workspaces/gitrepos/elder-apothecary/screens/deodorant.png',
           }}
         />
         <Card.Title>Products you can Trust</Card.Title>
@@ -42,24 +42,24 @@ function Home({ navigation }) {
         </Card.Title>
         <Button title='Learn More' />
       </Card>
-    </View>
+    </ScrollView>
   );
 }
 
 export default Home;
 
-// {/* <View }>
+// {/* <ScrollView }>
 //     <Text>Welcome to Elder Apothecary</Text>
 //     <Text>Find your favorite Apothecary products and more</Text>
 //     <Button title='Learn More' />
-//   </View>
-//   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//   </ScrollView>
+//   <ScrollView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 //     <Text>Products you can Trust</Text>
 //     <Text>Search multiple apothecaries for products you trust</Text>
 //     <Button title='Learn More' />
-//   </View>
-//   <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//   </ScrollView>
+//   <ScrollView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 //     <Text>Why Elder Apothecary?</Text>
 //     <Text>It's simple! We care about your health and your time.</Text>
 //     <Button title='Learn More' />
-//   </View> */}
+//   </ScrollView> */}
