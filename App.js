54a8644a57bb13225c9845d25c394958ec85d14d
Jacquelyn Elder <jacquelyn07@mac.com>
2021-03-24 15:10:17 -0500

    Organize and add screen components

--- App.js
+++ App.js
@@ -1,51 +1,25 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 
-function HomeScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home</Text>
-    </View>
-  );
-}
-
-function ContactScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Contact</Text>
-    </View>
-  );
-}
-
-function AboutScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>About Us</Text>
-    </View>
-  );
-}
-
-function BlogScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Blog</Text>
-    </View>
-  );
-}
+import Home from './screens/Home';
+import Contact from './screens/Contact';
+import About from './screens/About';
+import Blog from './screens/Blog';
 
 const Drawer = createDrawerNavigator();
 
-export default function App() {
+function App() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName='Home'>
-        <Drawer.Screen name='Home' component={HomeScreen} title='Home' />
-        <Drawer.Screen name='Contact Us' component={ContactScreen} />
-        <Drawer.Screen name='About Us' component={AboutScreen} />
-        <Drawer.Screen name='Blog' component={BlogScreen} />
+        <Drawer.Screen name='Home' component={Home} />
+        <Drawer.Screen name='Contact Us' component={Contact} />
+        <Drawer.Screen name='About Us' component={About} />
+        <Drawer.Screen name='Blog' component={Blog} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
+
+export default App;
---
+++ screens/Blog.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+
+function Blog({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Blog</Text>
+    </View>
+  );
+}
+
+export default Blog;
