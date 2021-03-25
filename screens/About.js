51a6b88130034279e5cd9e9e0069a111e7d97742
Jacquelyn Elder <jacquelyn07@mac.com>
2021-03-24 19:04:03 -0500

    Add Herbal medicine vs western medicine card

--- screens/About.js
+++ screens/About.js
@@ -4,8 +4,8 @@ import { Card } from 'react-native-elements';
 
 function About({ navigation }) {
   return (
-    <ScrollView style={{ marginTop: 40 }}>
-      <Card styles={{ padding: 10 }}>
+    <ScrollView style={{ marginTop: 80 }}>
+      <Card styles={{ padding: 30 }}>
         <Card.Title>
           'Light a fire of truth around your life, and all that is false in this
           world will cower at the sight of you.'
@@ -13,7 +13,7 @@ function About({ navigation }) {
         <Card.Title> ~ Mia Hollow ~ </Card.Title>
         <Card.Title> Quotes by Mia Hollow</Card.Title>
       </Card>
-      <Card styles={{ padding: 10 }}>
+      <Card styles={{ padding: 30 }}>
         <Card.Title>Our Mission</Card.Title>
         <Card.Title>
           Welcome to Elder Apothecary. We are so glad you are here. We want
@@ -25,15 +25,15 @@ function About({ navigation }) {
           find the balance in life as well as the healer within each and
           everyone of us.{' '}
         </Card.Title>
-        <Card.Title>About the Founder of Elder Apothecary: </Card.Title>
-        <Card.Title>
-          Ever since I was a child, I had a vision that I was a healer, and as
-          time went on I realized I was just that. What I learned along the way
-          is we are all healers, we just have to learn how to use our gifts. As
-          a society, we have been pushed so far from our roots as a species that
-          we are willing to take medicine that basically advertises that it is
-          Anti-Biological being. Let me explain what I mean{' '}
-        </Card.Title>
+      </Card>
+
+      <Card styles={{ padding: 30 }}>
+        <Card.Title>Herbal medicine:</Card.Title>
+        <Card.Title>Oregano oil</Card.Title>
+        <Card.Title>-Anti-microbial, anti-bacterial, anti-fungal-</Card.Title>
+        <Card.Title>Western Medicine:</Card.Title>
+        <Card.Title>Amoxicillin</Card.Title>
+        <Card.Title>-Antibiotic-</Card.Title>
       </Card>
     </ScrollView>
   );
