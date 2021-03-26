6d0af4674369d9b248847a3141b010c789d6baf4
Jacquelyn Elder <jacquelyn07@mac.com>
2021-03-26 10:59:46 -0500

    Add splash and change final colors

--- screens/Home.js
+++ screens/Home.js
@@ -4,54 +4,62 @@ import { Card, Button } from 'react-native-elements';
 import { colors } from '../colors';
 function Home({ navigation }) {
   return (
-    <ScrollView style={{ marginTop: 10, backgroundColor: colors.gray }}>
-      <Card>
+    <ScrollView style={{ marginTop: 10 }}>
+      <Card containerStyle={{ backgroundColor: colors.green }}>
         <Card.Image
           source={{
             uri:
               '/Users/jacquelynhagman/workspaces/gitrepos/elder-apothecary/screens/carousel-1800-1.jpg',
           }}
         />
-        <Card.Title>Welcome to Elder Apothecary</Card.Title>
-        <Card.Title>Find your favorite Apothecary products and more</Card.Title>
+        <Card.Title style={{ color: 'white' }}>
+          Welcome to Elder Apothecary
+        </Card.Title>
+        <Card.Title style={{ color: 'white' }}>
+          Find your favorite Apothecary products and more
+        </Card.Title>
         <Button
           title='Learn More'
           type='solid'
-          buttonStyle={{ backgroundColor: colors.green }}
+          buttonStyle={{ backgroundColor: colors.gray }}
         />
       </Card>
-      <Card>
+      <Card containerStyle={{ backgroundColor: colors.green }}>
         <Card.Image
           source={{
             uri:
               '/Users/jacquelynhagman/workspaces/gitrepos/elder-apothecary/screens/deodorant.png',
           }}
         />
-        <Card.Title>Products you can Trust</Card.Title>
-        <Card.Title>
+        <Card.Title style={{ color: 'white' }}>
+          Products you can Trust
+        </Card.Title>
+        <Card.Title style={{ color: 'white' }}>
           Search multiple apothecaries for products you trust
         </Card.Title>
         <Button
           title='Learn More'
           type='solid'
-          buttonStyle={{ backgroundColor: colors.green }}
+          buttonStyle={{ backgroundColor: colors.gray }}
         />
       </Card>
-      <Card>
+      <Card containerStyle={{ backgroundColor: colors.green }}>
         <Card.Image
           source={{
             uri:
               '/Users/jacquelynhagman/workspaces/gitrepos/elder-apothecary/screens/carousel-1800-3.jpeg',
           }}
         />
-        <Card.Title>Why Elder Apothecary?</Card.Title>
-        <Card.Title>
+        <Card.Title style={{ color: 'white' }}>
+          Why Elder Apothecary?
+        </Card.Title>
+        <Card.Title style={{ color: 'white' }}>
           It's simple! We care about your health and your time
         </Card.Title>
         <Button
           title='Learn More'
           type='solid'
-          buttonStyle={{ backgroundColor: colors.green }}
+          buttonStyle={{ backgroundColor: colors.gray }}
         />
       </Card>
     </ScrollView>
@@ -59,19 +67,3 @@ function Home({ navigation }) {
 }
 
 export default Home;
-
-// {/* <ScrollView }>
-//     <Text>Welcome to Elder Apothecary</Text>
-//     <Text>Find your favorite Apothecary products and more</Text>
-//     <Button title='Learn More' />
-//   </ScrollView>
-//   <ScrollView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//     <Text>Products you can Trust</Text>
-//     <Text>Search multiple apothecaries for products you trust</Text>
-//     <Button title='Learn More' />
-//   </ScrollView>
-//   <ScrollView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-//     <Text>Why Elder Apothecary?</Text>
-//     <Text>It's simple! We care about your health and your time.</Text>
-//     <Button title='Learn More' />
-//   </ScrollView> */}
